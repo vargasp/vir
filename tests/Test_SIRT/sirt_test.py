35a54f1fb8a8e75ea8d295c5847550269cc6b1c3
Phillip Vargas <vargasp@uchicago.edu>
2022-07-14 15:16:21 -0500

    Added TV to SIRT testing. Temp disabled projection.so

--- tests/Test_SIRT/sirt_test.py
+++ tests/Test_SIRT/sirt_test.py
@@ -10,25 +10,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage.data import shepp_logan_phantom
 from skimage.transform import radon, resize
-import importlib
+from skimage.restoration import denoise_tv_chambolle
 
-import siddons as sd
-import sirt
+import vir.siddon as sd
+import vir.projection as proj
 import vir
-import pyrm_funcs as pf
-import visualization_tools as vt
+import vt
 
- 
+def sirt(sino, sdlist, W_sino, W_img, nPixels, gamma=1, beta=0, iters=10):
+    
+    idx1 = np.nonzero(W_sino)
+    idx2 = np.nonzero(W_img)
+    image_iters = np.zeros(nPixels + (iters,), dtype=np.float32)
+    
+    for i in range(iters):
+        print("SIRT Iteration:",i)
+        sino_est = proj.sd_f_proj(image_iters[...,i-1],sdlist)
+        sino_est[idx1] = (sino[idx1] - sino_est[idx1])/W_sino[idx1]
+        
+        image_update = proj.sd_b_proj(sino_est,sdlist,nPixels)
+
+        image_update[idx2] /= W_img[idx2]
+        image_iters[...,i] = image_iters[...,i-1] + gamma*image_update
+    
+        if beta >0:
+            image_iters[...,i] = denoise_tv_chambolle(image_iters[...,i],weight=beta)
+    
+    return image_iters
 
 
 
 #Sheep Lgan Cicular
 nPix = 64
 nPixels = (nPix,nPix,1)
-dPix = 1.0
+dPix = 1.
 nDets = nPix
-dDet = 1.0
+dDet = 1.
 nTheta = nPix*2
+
+gamma = 1
 
 
 det_lets = 1
@@ -37,105 +57,38 @@ d = vir.Detector1d(nDets=nDets,dDet=dDet, det_lets=det_lets)
 Thetas = np.linspace(0,np.pi,nTheta*src_lets)
 Thetas = np.reshape(Thetas,(nTheta,src_lets))
 
-srcs, trgs = sd.circular_geom_st(d.Det2_lets, Thetas, 128)
-a = sd.siddons2(srcs,trgs,nPixels, dPix)
-inter_pix1,inter_len1 = sd.average_inter(a)
-inter_pix2 = sd.unravel_list(inter_pix1, inter_len1, nPixels)
+srcs, trgs = sd.circular_geom_st(d.Det2_lets, Thetas)
+sdlist = sd.siddons(srcs,trgs,nPixels, dPix).squeeze()
 
 Thetas = np.linspace(0,np.pi,nTheta)
 image = shepp_logan_phantom()
 image = resize(image,(nPix,nPix))
+image = image[:,:,np.newaxis]
 
-sino = sirt.forward_proj2(image, inter_pix2, inter_len2)
+sino  = proj.sd_f_proj(image, sdlist)
 
-images2 = sirt.SIRT_py2d(nPixels[0:2], sino, inter_pix2, inter_len2, iters=10, gamma=1.0)
-plt.imshow(images2[:,:,-1])
+I0 = 1e4
+I = I0*np.exp(-1* 0.167 * sino)
+I =  np.random.poisson(I)
+sino_n = np.log(I/I0)/ -0.167 
+ 
+ 
+ 
+W_sino =  proj.sd_f_proj(np.ones(nPixels), sdlist)
+W_img = proj.sd_b_proj(np.ones(sino.shape), sdlist, nPixels)
 
+iters = 500
+rec0 = sirt(sino_n, sdlist, W_sino, W_img, nPixels, gamma, beta=0,iters=iters)
+recT = sirt(sino_n, sdlist, W_sino, W_img, nPixels, gamma, beta=1e-4,iters=iters)
 
-images3 = sirt.SIRT_py2d(nPixels, sino, inter_pix2, inter_len2, iters=10, gamma=1.0)
-plt.imshow(images3[:,:,0,-1])
-
-
-
-
-
-
-
-
-
-
-
-
-
+#vt.CreateImage(sino)
+vt.CreateImage(recT[:,:,0,iters-1])
 
 
-#Sheep Lgan Perp
-g = pf.Geom2D()
-d = pf.Detector(nPixels,dPix,dBin=1.0,nBinsY=nPix,nBinsZ=1)
-
-srcX = nPix * dPix
-DetX = -27* (nPix*dPix)/(250./.96)
-DetY = d.UnqDetYs
-srcY = np.add.outer(np.tan(g.Phi)*(srcX - DetX),DetY)
-srcZ = 0.0
-DetZ = 0.0
-
-trg = np.stack([np.repeat(srcX,512), np.squeeze(srcY), np.repeat(srcZ,512)]).T
-src = np.array([DetX,DetY[0],DetZ])
-a = sd.siddons2(src,trg,nPixels, dPix)
-inter_pix,inter_len = sd.siddons_list2arrays(a)
-
-
-
-
-srcX = nPix * dPix
-DetX = -27* (nPix*dPix)/(250./.96) - (nPix*dPix)/2.0
-DetY = d.DetYs
-srcY = np.add.outer(np.tan(g.Phi)*(srcX - DetX),DetY)
-srcZ = 0.0
-DetZ = 0.0
-
-trg1 = np.stack([np.full(srcY.shape,srcX),srcY,np.full(srcY.shape,srcZ)]).T
-src1 = np.array([np.full(srcY.shape,DetX),np.tile(DetY,[512,1]),np.full(srcY.shape,DetZ)]).T
-
-trg2 = np.copy(trg[:,:,[1,0,2]])
-src2 = np.copy(src[:,:,[1,0,2]])
-
-trg = np.vstack([trg1,trg2])
-src = np.vstack([src1,src2])
-
-
-a = sd.siddons2(src,trg,nPixels, dPix)
-inter_pix,inter_len = sd.siddons_list2arrays(a)
-
-W_sino, W_img =  sirt.SIRT_weights(nPixels, inter_pix, inter_len)
-
-
-
-image = shepp_logan_phantom()
-image = resize(image,(nPix,nPix))
-
-sino = sirt.forward_proj2(image, inter_pix, inter_len)
-
-image = sirt.back_proj2(sino, inter_pix, inter_len,nPixels)
-
-wow = sirt.SIRT_py2d(nPixels[0:2], sino, inter_pix, inter_len, iters=10, gamma=1.0)
-
-
-
-W_sino, W_img = sirt.SIRT_weights(nPixels, inter_pix, inter_len)
-
-iters = 10
-gamma = 100
-image_iters = np.zeros(nPixels + (iters,))
-image_iters[:,:,0] = np.zeros(nPixels)
-
-i=0
-sino_est = sirt.forward_proj2(image_iters[:,:,i-1], inter_pix, inter_len)
-image_update = sirt.back_proj2((sino - sino_est)/W_sino, inter_pix, inter_len, nPixels)/W_img
-image_iters[:,:,i] = image_iters[:,:,i-1] + gamma*image_update
-
-
-
+pT = image[:,32,0]
+pR = rec0[:,32,0,iters-1]
+pV = recT[:,32,0,iters-1]
+Ps = np.vstack([pT,pR,pV]).T
+vt.CreatePlot(Ps)
 
 
