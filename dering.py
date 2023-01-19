fa78300f2cb9decc1c07d6f76a118ef021a3cb08
Phil Vargas <vargasp@uchicago.edu>
2023-01-18 20:35:10 -0600

    Update dering.py

--- dering.py
+++ dering.py
@@ -47,32 +47,77 @@ def polar_to_cart(polar_data, nX, nY, theta_step=1, range_step=1,order=3):
     return(cart_data.reshape(nY, nX))
 
 
+def linear_kernel(k_max, width, steps=3):
+    steps_max = int(np.ceil(k_max/2))
+    steps = np.min([steps+1, steps_max])
+    
+    steps_filt = np.floor(np.linspace(1,steps,width,endpoint=False))
+
+    return  2*np.floor(k_max*steps_filt/(steps-1)/2).astype(int) + 1
+
+
+def polar_filter(img, kernel_max, axis):
+    k_size = img.shape[1]
+    
+    kernel_filter = linear_kernel(kernel_max, k_size)
+    
+    kernel = [1,1]
+    
+    lf_img = np.zeros(img.shape)
+    
+    
+    for k in np.unique(kernel_filter):
+        print(k)
+        
+        kernel[axis] = k
+        f_img = medfilt(img, kernel)
+        
+        idx = np.where(kernel_filter == k)[0]
+        lf_img[:,idx] = f_img[:,idx]
+        
+    return lf_img
+
+
 def dering(img,threshold_min=None,threshold_max=None,threshold_art=30,\
-           azimuthal_kernel=11,radial_kernel=21):
+           azimuthal_kernel=11,radial_kernel=21, return_art=False):
 
     nX, nY = img.shape
     
+    #Lower threshold for image segmentation
     if threshold_min is None:
         threshold_min = img.min()
 
+    #Upper threshold for image segmentation
     if threshold_max is None:
         threshold_max = img.max()
 
-    t_img = img.clip(threshold_min,threshold_max)
+    #Thresholded image
+    tImg = img.clip(threshold_min,threshold_max)
 
-    img_p = warp_polar(t_img)
+    #Image transformed into polar coordinates
+    nRadii = int(np.ceil(np.sqrt((nX/2)**2 + (nY/2)**2)))
+    nThetas = 360
+    pImg = warp_polar(tImg, output_shape=[nThetas,nRadii])
 
+    #Radial median filtering in polar coordinates
+    fImg = medfilt(pImg, [1,radial_kernel])
+    
+    #Subtract median imge to calculate artifactimge.
+    #Thresholded artifact image
+    fImg = (pImg - fImg).clip(0,threshold_art)
 
-    dImg = medfilt(img_p, [1,radial_kernel])
-    dImg = (img_p - dImg).clip(0,threshold_art)
-    dImg = medfilt(dImg, [azimuthal_kernel,1])
+    #Azimuthal filtering in polar coordinates
+    dImg = medfilt(fImg, [azimuthal_kernel,1])
 
+    #Artifact Image transformed into cartesian coordinates
     ring_art = polar_to_cart(dImg,nX,nY)
 
-    return(img - ring_art)
 
-           
-           
+
+    if return_art:
+        return img - ring_art, ring_art
+    else:
+        return img - ring_art
            
            
            
