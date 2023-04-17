Machine precision fix matching rounding in alphas to epsilon

siddon/siddon.py
@@ -511,7 +511,8 @@ def siddons(src, trg, nPixels=128, dPixels=1.0, origin=0.0,\
     
     #Machine precision
     epsilon = 1e-8
-   
+    decimal_round = int(-np.log(epsilon))
+    
     #Creates the grid of voxels
     g = vir.Grid3d(nPixels=nPixels,dPixels=dPixels,origin=origin)
 
@@ -575,6 +576,7 @@ def siddons(src, trg, nPixels=128, dPixels=1.0, origin=0.0,\
                   np.ceil((trg + alpha_max[...,np.newaxis]*dST - p0)/g.dPixels).astype(int),\
                   np.ceil((trg + alpha_min[...,np.newaxis]*dST - p0)/g.dPixels).astype(int))
 
+
     #Loops through the rays intersecting the source and target
     for ray_idx, ray in np.ndenumerate(nRays):
         
@@ -594,7 +596,7 @@ def siddons(src, trg, nPixels=128, dPixels=1.0, origin=0.0,\
             #Merges and sorts alphas
             #Alpha = np.sort(np.concatenate([alpha_bounds[ray,:], X_alpha, Y_alpha, Z_alpha]), kind='mergesort')
             #Rounding function was added to elimintae duplicate alphas introduced by machine precision
-            Alpha = np.unique(np.round(np.concatenate([alpha_bounds[ray_idx], X_alpha, Y_alpha, Z_alpha]),decimals=12))
+            Alpha = np.unique(np.round(np.concatenate([alpha_bounds[ray_idx], X_alpha, Y_alpha, Z_alpha]),decimals=decimal_round))
 
             #Loops through the alphas and calculates pixel length and pixel index
             dAlpha = Alpha[1:] - Alpha[:-1]
@@ -605,13 +607,6 @@ def siddons(src, trg, nPixels=128, dPixels=1.0, origin=0.0,\
             z_ind = ((trg[idxZ] + mAlpha*dST[idxZ] - g.Zb[0])/g.dZ).astype(int)
            
             length = (distance[ray_idx]*dAlpha).astype(np.float32)
-
-            #TEMP FIX
-            x_ind = x_ind.clip(0,249)
-            y_ind = y_ind.clip(0,499)
-            z_ind = z_ind.clip(0,159)
-
-
             #Stores the index and intersection length in flat, raveled, or
             #unraveled form
             if flat == True:

tests/Test_Siddons/siddons_test2.py
@@ -35,8 +35,8 @@ def plot_params(nPix, dPix, src, trg):
 nPix = (12,10,1)
 dPix = (.8, 1,1)
 
-src = np.array((-2.5, .25,0))
-trg = np.array((2,1.25, 0))
+src = np.array((-6.5, .25,0))
+trg = np.array((6.5,1.25, 0))
 
 
 sdlist = sd.siddons(trg, src, nPixels=nPix, dPixels=dPix)
@@ -85,6 +85,22 @@ b = sdarray
 x0,xN,y0,yN,xS,yS = plot_params(nPix, dPix, src, trg)
 plt.imshow(sdarray[:,:,0].T, extent=(x0,xN,y0,yN), origin='lower')
 plt.show()
+
+
+
+
+src = np.array([-141.38,0.49, 0.49])
+trg = np.array([120., 176.66012, 176.66012])
+sdlist = sd.siddons(trg, src, nPixels=(250,500,160), dPixels=.96, origin=(0, 0, 76.8),flat = True, ravel= True)
+
+
+
+sd.siddons(Srcs, Trgs, nPixels=nPixelsG, dPixels=dPixel, origin=origin,\
+            flat = True, ravel= True)
+
+
+
+
 
 
 