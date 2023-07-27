2618052b1c76c51e98c74c31dbb63c0253485506
Phillip Vargas <vargasp@uchicago.edu>
2023-07-26 21:32:36 -0500

    Update psf_test.py

--- tests/Test_PSF/psf_test.py
+++ tests/Test_PSF/psf_test.py
@@ -16,23 +16,25 @@ import vir
 import vir.psf as psf
 import vir.fwhm as fwhm
 
+from scipy import ndimage
 
-def moments(x,y, data):
+
+def moments(imp):
     """Returns (height, x, y, width_x, width_y)
     the gaussian parameters of a 2D distribution by calculating its
     moments """
-    total = data.sum()
-    X, Y = np.meshgrid(x,y)
     
-    
-    x = (X*data).sum()/total
-    y = (Y*data).sum()/total
-    col = data[:, int(y)]
-    width_x = np.sqrt(np.abs((np.arange(col.size)-x)**2*col).sum()/col.sum())
-    row = data[int(x), :]
-    width_y = np.sqrt(np.abs((np.arange(row.size)-y)**2*row).sum()/row.sum())
-    height = data.max()
-    return height, x, y, width_x, width_y
+    A = imp.max()
+    x, y  = ndimage.center_of_mass(imp)
+
+    profile_x = imp[:, int(y)]
+    profile_y = imp[int(x), :]
+
+    #Estimate sigma by the root mean squared deviation
+    sigma_x = np.sqrt(np.abs((np.arange(profile_x.size)-x)**2*profile_x).sum()/profile_x.sum())
+    sigma_y = np.sqrt(np.abs((np.arange(profile_y.size)-y)**2*profile_y).sum()/profile_y.sum())
+
+    return x, y, sigma_x, sigma_y, A
 
 
 def fitGaussian2d(x, y, data):
@@ -44,6 +46,20 @@ def fitGaussian2d(x, y, data):
     x = np.ravel(x)
     y = np.ravel(y)
     data = np.ravel(data)
+    p = curve_fit(psf.gaussinfunc2d, np.array((x,y)), z,p0=p0)
+    
+
+
+N = 15
+n = vir.censpace(N)
+x2, y2 = np.meshgrid(n,n)
+x3, y3, z3 = np.meshgrid(n,n,n)
+
+imp2d = psf.gaussian2d(nX=N,nY=N, mus=(5,-2), sigmas=(.5,1))
+print(moments(imp2d))
+
+
+imp3d = psf.gaussian3d(nX=N,nY=N,nZ=N)
 
     
 
@@ -52,11 +68,8 @@ p = curve_fit(psf.gaussinfunc2d, np.array((x,y)), z,p0=p0)
 
 
 
-N = 25
-imp2d = psf.gaussian2d(nX=N,nY=N)
-imp3d = psf.gaussian3d(nX=N,nY=N,nZ=N)
 
-n = vir.censpace(N)
-x2, y2 = np.meshgrid(n,n)
-x3, y3, z3 = np.meshgrid(n,n,n)
+
+
+
 
