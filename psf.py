fec0d4f99667df44955641b515ddf93569e4af6e
Phillip Vargas <vargasp@uchicago.edu>
2023-08-03 17:40:16 -0500

    Update psf.py

--- psf.py
+++ psf.py
@@ -11,86 +11,77 @@ import scipy.spatial.transform as transform
 
 import vir
 
-"""
 
 
-def gaussian2d(mu=(0.0,0.0), sigma=(5.0,5.0), theta=0, A=None, \
-               nX=128, nY=128):
-    
-    X = vir.censpace(nX)
-    Y = vir.censpace(nY)
+def gaussinfunc1d(x, mu, sigma, A):
+    """
+    Returns the pdf of a 1d guassian distribution. Guassian paramters are
+    scalar so the function can be used in model fitting algorithms. 
 
-    mu = np.array(mu)
-    sigma = np.array(sigma)
-       
+    Parameters
+    ----------
+    x : float or np.array
+        The independent value(s) of the function.
+    mu : float
+        Mean value of the function.
+    sigma : float
+        Standard deviations of the function
+    A : float
+        The amplitude of the function
 
-    c, s = np.cos(theta), np.sin(theta)
-    R  = np.array([[c, -s], [s, c]])
-    
-    #R = transform.Rotation.from_euler('z', theta, degrees=True).as_matrix()[:2,:2]
-    
-    COV = R @ np.diag((1/sigma)**2) @ R.T
-    
-    V2 = np.dstack(np.meshgrid(X,Y))
-    
-    if A is None:
-        A = 1.0 / (np.prod(sigma) * (2*np.pi))
-    
-    return A * np.exp(-0.5*((V2 @ COV) * V2).sum(axis=2))
-
-
-def gaussian3d(mu=(0.0,0.0,0.0), sigma=(5.0,5.0,5.0), angles=(0.0,0.0,0.0),\
-               A=None, nX=128, nY=128, nZ=128):
-    
-    X = vir.censpace(nX)
-    Y = vir.censpace(nY)
-    Z = vir.censpace(nZ)
-
-    mu = np.array(mu)
-    sigma = np.array(sigma)
-       
-    R = transform.Rotation.from_euler('xyz', angles, degrees=True).as_matrix()
-    COV = R @ np.diag((1/sigma)**2) @ R.T
-    
-    V2 = np.stack(np.meshgrid(X,Y,Z), axis=3)
-    
-    if A is None:
-        A = 1.0 / (np.prod(sigma) * (2*np.pi)**(3/2))
-    
-    return A * np.exp(-0.5*((V2 @ COV) * V2).sum(axis=3))
-
-
-
-"""
-
-
-def gaussinfunc1d(x, mu, sigma, A):            
+    Returns
+    -------
+    float or np.array
+        The value of array of values of the pdf at x.
+    """
     return A*np.exp(-(x - mu)**2/(2*sigma**2))
 
 
-def gaussinfunc2d(xy, x_mu, y_mu, x_sigma, y_sigma,A):   
-    x,y = xy
+def gaussinfunc2d(xy, x_mu, y_mu, x_sigma, y_sigma, theta, A):
+    """
+    Returns the pdf of a 2d guassian distribution. This is a general case where
+    the axes may not be aligned with the coordiante system. Guassian paramters
+    are scalar so the function can be used in model fitting algorithms. 
+
+    Parameters
+    ----------
+    xy : array of 2 floats or 2 np.arrays
+        The independent value(s) x and y of the function.
+    x_mu : TYPE
+        Mean value in the x dimension of the function.
+    y_mu : float
+        Mean value in the y dimension of the function.
+    x_sigma : float
+        Standard deviations in the x dimension of the function
+    y_sigma : float
+        Standard deviations in the y dimension of the function
+    theta : float
+        Angle the axis along x_sigma makes with the positive x axis in radians
+    A : float
+        The amplitude of the function
+
+    Returns
+    -------
+    float or np.array
+        The value of array of values of the pdf at x and y.
+    """
     
-    return A*np.exp(-(x - x_mu)**2/(2*x_sigma**2) \
-                    -(y - y_mu)**2/(2*y_sigma**2))
-
-
-def gaussinfunc3d(xyz, x_mu, y_mu, z_mu, x_sigma, y_sigma, z_sigma,A):            
-    x,y,z = xyz         
-
-    return A*np.exp(-(x - x_mu)**2/(2*x_sigma**2) \
-                    -(y - y_mu)**2/(2*y_sigma**2) \
-                    -(z - z_mu)**2/(2*z_sigma**2))
-
-def gaussinfunc2dG(xy, x_mu, y_mu, x_sigma, y_sigma, theta, A):   
+    #Creates np.arrays for elementwise arithmetic
     mu = np.array([x_mu, y_mu])
     sigma = np.array([x_sigma, y_sigma])
-       
-    V = xy - mu
 
-    R = transform.Rotation.from_euler('z', theta).as_matrix()[:2,:2]       
-    COV = R @ np.diag((1/sigma)**2) @ R.T
-        
+    #Translates the impulse to mean location       
+    V = np.stack(xy, axis=-1) - mu
+
+    #Generates the rotation matrix
+    #scipy's transform's is 3x3, truncated to transform Nx2 matrices
+    R = transform.Rotation.from_euler('z', theta).as_matrix()[:2,:2]
+    
+    #Creates the covariance matrix
+    #OPTIMIZE This can probably be sped up with np.einsum notation
+    COV = R @ np.diag((1.0/sigma)**2) @ R.T
+    
+    #Returns the gaussian pdf
     return A * np.exp(-0.5*((V @ COV) * V).sum(axis=2))
         
 
@@ -98,21 +89,92 @@ def gaussinfunc3dG(xyz, x_mu, y_mu, z_mu, \
                    x_sigma, y_sigma, z_sigma, \
                    theta, phi, psi, A):
 
-    mu = np.array([x_mu, y_mu])
-    sigma = np.array([x_sigma, y_sigma])
+    #Creates np.arrays for elementwise arithmetic
+    mu = np.array([x_mu, y_mu, z_mu])
+    sigma = np.array([x_sigma, y_sigma, z_sigma])
+
+    #Translates the impulse to mean location       
+    V =  np.stack(xyz, axis=-1) - mu
        
-    
+    #Generates the rotation matrix    
     R = transform.Rotation.from_euler('xyz', [theta,phi,psi]).as_matrix()
-    COV = R @ np.diag((1/sigma)**2) @ R.T
+
+    #Creates the covariance matrix
+    #OPTIMIZE This can probably be sped up with np.einsum notation
+    COV = R @ np.diag((1.0/sigma)**2) @ R.T
     
-    V2 = np.stack(np.meshgrid(X,Y,Z), axis=3)
+    #Returns the gaussian pdf    
+    return A * np.exp(-0.5*((V @ COV) * V).sum(axis=3))
+
+
+def gaussinfunc2d_ortho(xy, x_mu, y_mu, x_sigma, y_sigma, A):
+    """
+    Returns the pdf of a 2d guassian distribution. This is a special case where
+    the axes are aligned with the coordiante system. Guassian paramters are
+    scalar so the function can be used in model fitting algorithms. 
     
-    if A is None:
-        A = 1.0 / (np.prod(sigma) * (2*np.pi)**(3/2))
+    Parameters
+    ----------
+    xy : array of 2 floats or 2 np.arrays
+        The independent value(s) x and y of the function.
+    x_mu : TYPE
+        Mean value in the x dimension of the function.
+    y_mu : float
+        Mean value in the y dimension of the function.
+    x_sigma : float
+        Standard deviations in the x dimension of the function
+    y_sigma : float
+        Standard deviations in the y dimension of the function
+    A : float
+        The amplitude of the function
+
+    Returns
+    -------
+    float or np.array
+        The value of array of values of the pdf at x and y.
+    """
+    x,y = xy
     
-    return A * np.exp(-0.5*((V2 @ COV) * V2).sum(axis=3))
+    return A*np.exp(-(x - x_mu)**2/(2*x_sigma**2) \
+                    -(y - y_mu)**2/(2*y_sigma**2))
+
+
+def gaussinfunc3d_orth0(xyz, x_mu, y_mu, z_mu, x_sigma, y_sigma, z_sigma, A):
+    """
+    Returns the pdf of a 3d guassian distribution. This is a special case where
+    the axes are aligned with the coordiante system. Guassian paramters are
+    scalar so the function can be used in model fitting algorithms. 
     
-    
+    Parameters
+    ----------
+    xyz : array of 3 floats or 3 np.arrays
+        The independent value(s) x, y  and z of the function.
+    x_mu : TYPE
+        Mean value in the x dimension of the function.
+    y_mu : float
+        Mean value in the y dimension of the function.
+    z_mu : float
+        Mean value in the z dimension of the function.
+    x_sigma : float
+        Standard deviations in the x dimension of the function
+    y_sigma : float
+        Standard deviations in the y dimension of the function
+    z_sigma : float
+        Standard deviations in the z dimension of the function
+    A : float
+        The amplitude of the function
+
+    Returns
+    -------
+    float or np.array
+        The value of array of values of the pdf at x, y and z.
+    """
+    x,y,z = xyz         
+
+    return A*np.exp(-(x - x_mu)**2/(2*x_sigma**2) \
+                    -(y - y_mu)**2/(2*y_sigma**2) \
+                    -(z - z_mu)**2/(2*z_sigma**2))
+
 
 def gaussian1d(mu=0.0, sigma=5.0, A=None, nX=128):
     """
@@ -144,7 +206,7 @@ def gaussian1d(mu=0.0, sigma=5.0, A=None, nX=128):
         A = 1.0 / (sigma*np.sqrt(2*np.pi))
             
     #Returns the kernel
-    return A*np.exp(-(x - mu)**2/(2*sigma**2))
+    return A* gaussinfunc1d(x, mu, sigma, A)
 
 
 def gaussian2d(mus=(0.0,0.0), sigmas=(5.0,5.0), theta=0, A=None, \
@@ -157,15 +219,10 @@ def gaussian2d(mus=(0.0,0.0), sigmas=(5.0,5.0), theta=0, A=None, \
     y = vir.censpace(nY)
     x, y = np.meshgrid(x,y)
     
-    a = np.cos(theta)**2 / (2*x_sigma**2) + np.sin(theta)**2 / (2*y_sigma**2)
-    b = -np.sin(2*theta) / (4*x_sigma**2) + np.sin(2*theta) / (4*y_sigma**2)
-    c = np.sin(theta)**2 / (2*x_sigma**2) + np.cos(theta)**2 / (2*y_sigma**2)
-    
     if A is None:
         A = 1.0 / (x_sigma*y_sigma * (2*np.pi))
-    
-    return A*np.exp(-a*(x - x_mu)**2 - 2*b*(x - x_mu)*(y - y_mu) - c*(y - y_mu)**2)
-    
+
+    return A* gaussinfunc2d((x,y), x_mu, y_mu, x_sigma, y_sigma, theta, A)
 
 
 def gaussian3d(mus=(0.0,0.0,0.0), sigmas=(5.0,5.0,5.0), A=None, \
@@ -336,4 +393,83 @@ def polyToParams3D(vec,printMe=False):
    inve=np.linalg.inv(ec) #inverse is actually the transpose here
    #if printMe: print('\nRotation matrix\n',inve)
    return (center,axes,inve)
+
+
+
+
+"""
+!!!Deprecated functions!!!
+
+
+def gaussian2d(mus=(0.0,0.0), sigmas=(5.0,5.0), theta=0, A=None, \
+               nX=128, nY=128):
+    
+    x_mu,y_mu = mus
+    x_sigma, y_sigma = sigmas
+    
+    x = vir.censpace(nX)
+    y = vir.censpace(nY)
+    x, y = np.meshgrid(x,y)
+    
+    a = np.cos(theta)**2 / (2*x_sigma**2) + np.sin(theta)**2 / (2*y_sigma**2)
+    b = -np.sin(2*theta) / (4*x_sigma**2) + np.sin(2*theta) / (4*y_sigma**2)
+    c = np.sin(theta)**2 / (2*x_sigma**2) + np.cos(theta)**2 / (2*y_sigma**2)
+    
+    if A is None:
+        A = 1.0 / (x_sigma*y_sigma * (2*np.pi))
+    
+    return A*np.exp(-a*(x - x_mu)**2 - 2*b*(x - x_mu)*(y - y_mu) - c*(y - y_mu)**2)
+
+
+def gaussian2d(mu=(0.0,0.0), sigma=(5.0,5.0), theta=0, A=None, \
+               nX=128, nY=128):
+    
+    X = vir.censpace(nX)
+    Y = vir.censpace(nY)
+
+    mu = np.array(mu)
+    sigma = np.array(sigma)
+       
+
+    c, s = np.cos(theta), np.sin(theta)
+    R  = np.array([[c, -s], [s, c]])
+    
+    #R = transform.Rotation.from_euler('z', theta, degrees=True).as_matrix()[:2,:2]
+    
+    COV = R @ np.diag((1/sigma)**2) @ R.T
+    
+    V2 = np.dstack(np.meshgrid(X,Y))
+    
+    if A is None:
+        A = 1.0 / (np.prod(sigma) * (2*np.pi))
+    
+    return A * np.exp(-0.5*((V2 @ COV) * V2).sum(axis=2))
+
+
+def gaussian3d(mu=(0.0,0.0,0.0), sigma=(5.0,5.0,5.0), angles=(0.0,0.0,0.0),\
+               A=None, nX=128, nY=128, nZ=128):
+    
+    X = vir.censpace(nX)
+    Y = vir.censpace(nY)
+    Z = vir.censpace(nZ)
+
+    mu = np.array(mu)
+    sigma = np.array(sigma)
+       
+    R = transform.Rotation.from_euler('xyz', angles, degrees=True).as_matrix()
+    COV = R @ np.diag((1/sigma)**2) @ R.T
+    
+    V2 = np.stack(np.meshgrid(X,Y,Z), axis=3)
+    
+    if A is None:
+        A = 1.0 / (np.prod(sigma) * (2*np.pi)**(3/2))
+    
+    return A * np.exp(-0.5*((V2 @ COV) * V2).sum(axis=3))
+
+
+
+"""
+
+
+
 
