72b0aed34cfc8398394f5c71babe4a48acaad088
Phillip Vargas <vargasp@uchicago.edu>
2023-08-10 17:37:50 -0500

    Update vos.py

--- vos.py
+++ vos.py
@@ -9,29 +9,47 @@ Created on Thu Dec  8 16:23:54 2022
 #from os.path import join, sep, abspath
 
 import os
+import re
 
-
-def file_path(fname,home=True):
+def file_path(fname):
     home_dir = os.path.expanduser('~')
     #work_dir = os.getcwd()
     
     #If fname correctly starts with home_dir assume fname is a correctly setup
     if fname.find(home_dir) == 0:
-        return fname
-        
-    #Remove seperators to correct for them in os.path.join
-    if fname.count('\\') > 0:
-        dirs = fname.split('\\')
-    elif fname.count('/') > 0:
-        dirs = fname.split('/')
+        fname = fname.replace(home_dir,'')
     else:
-        dirs = list(fname)
+        home_dir = ''
     
+    #Remove seperators to correct for them in os.path.join
+    dirs = re.split('\\\\|/',fname)
+    
+    return os.path.join(home_dir,*dirs)
+    """
     if home:
         return os.path.join(home_dir,*dirs)
     else:
         return os.path.join('/',*dirs)
-        
+     """  
+    
+def box_dir():
+
+    box_drive_location1 = 'C:\\Users\\vargasp\\Box\\'
+    box_drive_location2 = '/Users/pvargas21/Library/CloudStorage/Box-Box/'
+    box_drive_location3 = '/Users/vargasp/Library/CloudStorage/Box-Box/'
+
+    if os.path.exists(box_drive_location1):
+        return box_drive_location1
+
+    if os.path.exists(box_drive_location2):
+        return box_drive_location2
+
+    if os.path.exists(box_drive_location3):
+        return box_drive_location3
+
+    return ''
+
+
 
 """
 
