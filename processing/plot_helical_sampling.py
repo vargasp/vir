a3d287a0548c099e9f448c2890916a5480a4d4e5
Phillip Vargas <vargasp@uchicago.edu>
2023-09-08 11:36:52 -0500

    helical sampling graph update

--- processing/plot_helical_sampling.py
+++ processing/plot_helical_sampling.py
@@ -7,68 +7,52 @@ Created on Mon Apr  5 21:18:17 2021
 """
 
 
-import importlib
-
 import numpy as np
 import matplotlib.pyplot as plt
-import visualization_tools as vt
+from matplotlib.ticker import MultipleLocator, FuncFormatter
+
 import vir 
 
-from matplotlib.ticker import FormatStrFormatter, MultipleLocator,FuncFormatter
 
 
 def createHelicalSampleImage(Views, Z, CN=None, title=None,outfile=None, \
-                             y360=None, y180=None,base_tick=np.pi/2):
-    """
-    
+                             y360=None, y180=None,base_tick=np.pi/2,xlim=np.pi*2):
 
-    Parameters
-    ----------
-    Views : TYPE
-        DESCRIPTION.
-    Z : TYPE
-        DESCRIPTION.
-    y360 : (2) array_like, optional
-        Y locations for the extent of 2pi reconstruction. The default is None.
-    y180 : TYPE, optional
-        DESCRIPTION. The default is None.
-
-    Returns
-    -------
-    None.
-
-    """
-    print("h2")
     fig, ax = plt.subplots()
     if CN is None:
-        ax.plot(Views, Z, "b.")
+        ax.plot(Z, Views, "b.")
+
     else:
         color_l = ["b","g","r","c","m","y"]
 
         for i, color in enumerate(np.unique(CN)):
             idx = np.where(CN == color)
-            ax.scatter(Views[idx], Z[idx], c=color_l[i], label=color, marker=".")
-        
-    ax.xaxis.set_major_formatter(FuncFormatter(lambda val, pos: \
+            ax.scatter(Z[idx],Views[idx], c=color_l[i], label=color, marker=".")
+    
+    ax.yaxis.set_major_formatter(FuncFormatter(lambda val, pos: \
             '{0}$\pi$'.format(val/np.pi) if val !=0 else '0'))
-    ax.xaxis.set_major_locator(MultipleLocator(base=base_tick))
+    ax.yaxis.set_major_locator(MultipleLocator(base=base_tick))
+    ax.set_ylim(-base_tick/4.,xlim)
+    #ax.set_xlim(-20,20)
+
+
 
     #Creates shaded regions of 2pi reconstrion
     if y180:
-        ax.axhspan(y180[0], y180[1], alpha=0.2, color='yellow')
-        ax.axhline(y180[0], color='grey')
-        ax.axhline(y180[1], color='grey')
+        ax.axvspan(y180[0], y180[1], alpha=0.2, color='yellow')
+        ax.axvline(y180[0], color='grey')
+        ax.axvline(y180[1], color='grey')
 
     #Creates shaded regions of 2pi reconstrion
     if y360:
-        ax.axhspan(y360[0], y360[1], alpha=0.2, color='green')
-        ax.axhline(y360[0], color='grey')
-        ax.axhline(y360[1], color='grey')
+        ax.axvspan(y360[0], y360[1], alpha=0.2, color='green')
+        ax.axvline(y360[0], color='grey')
+        ax.axvline(y360[1], color='grey')
 
 
     ax.set_title(title, fontsize=14,fontname='Times New Roman')
-    ax.set_xlabel('Angle', fontsize=12,fontname='Times New Roman')
-    ax.set_ylabel('Z position', fontsize=12,fontname='Times New Roman')
+    ax.set_ylabel('Angle', fontsize=12,fontname='Times New Roman')
+    ax.set_xlabel('Z position', fontsize=12,fontname='Times New Roman')
 
     plt.savefig(outfile +'.pdf',format='pdf',bbox_inches='tight')
     plt.close(fig)
@@ -94,7 +78,7 @@ def scatter_samp(g,d, wrap="None"):
         outfile = "P1_p" +str(g.pitch)
         print("Here")
         createHelicalSampleImage(Views, Z, title=title,outfile=outfile, \
-                            y360=(y360l,y360r), y180=(y180l,y180r))
+                            y360=(y360l,y360r), y180=(y180l,y180r), xlim=g.coverage)
        
     else:
         VN, ZN, CN = np.array([]), np.array([]), np.array([])
@@ -117,15 +101,13 @@ def scatter_samp(g,d, wrap="None"):
             outfile = "P3_p" +str(g.pitch)
 
             createHelicalSampleImage(VN, ZN, CN=CN, title=title,outfile=outfile, \
-                             y180=(y180l,y180r))
+                             y180=(y180l,y180r),xlim=np.pi,base_tick=np.pi/4.)
         else:
             title ='2pi Wrapped Views: Pitch: '+str(g.pitch/d.nH/d.dH)
             outfile = "P2_p" +str(g.pitch)
 
             createHelicalSampleImage(VN, ZN, CN=CN, title=title,outfile=outfile, \
-                             y360=(y360l,y360r), y180=(y180l,y180r))
-
-    #return VN, ZN, CN
+                             y360=(y360l,y360r), y180=(y180l,y180r),xlim=np.pi*2)
 
 
 def helical_recon_range(g,d):
@@ -138,33 +120,20 @@ def helical_recon_range(g,d):
 
 
 
-dDet = 1.0 #[Micronns]
+dDet = 1.0
 nDets = (8,8)
 coverage = 4*np.pi
-nAngles = 16
-nViews = nAngles*coverage / (2.0*np.pi) + 1
-pitches = np.linspace(1,16,481)
-pitches = [0.875]
+nAngles = 10
+nViews = nAngles*coverage / (2.0*np.pi) 
+pitch = 8
 
-
-zp = vir.censpace(193,.0625)
-
-d = vir.Detector2d(nDets=nDets,dDet=dDet,offset=0.0,det_lets=10)
-
-
-g = vir.Geom(nViews=nViews,pitch=7, coverage=coverage,endpoint=True)
-#VN, ZN, CN = scatter_samp(g,d, wrap="None")
-
-
-"""
-for p_idx, pitch in enumerate(pitches):
-    g = vir.Geom(nViews=nViews,pitch=pitch, coverage=coverage,endpoint=True)
-    scatter_samp(g,d, wrap="None")
-"""
-
+d = vir.Detector2d(nDets=nDets,dDet=dDet,offset=0.0)
+g = vir.Geom(nViews=nViews,pitch=pitch, coverage=coverage,endpoint=False)
 
 
 scatter_samp(g,d, wrap="None")
+scatter_samp(g,d, wrap="2pi")
+scatter_samp(g,d, wrap="pi")
     
 
 
