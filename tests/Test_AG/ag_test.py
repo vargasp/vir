ec6d15e7ab7511ec14235343750e3197772cad41
Phillip Vargas <vargasp@uchicago.edu>
2023-11-28 08:46:22 -0600

    Update ag_test.py

--- tests/Test_AG/ag_test.py
+++ tests/Test_AG/ag_test.py
@@ -14,8 +14,45 @@ import vir
 import vir.analytic_geom as ag
 
 
-def pyramid_sa(a1,a2):
-   return 4*np.arcsin(sin(a1/2) * sin(a2/2)) 
+def sa_pyramid(angle1,angle2):
+   return 4*np.arcsin(np.sin(angle1/2) * np.sin(angle2/2)) 
+
+
+def da_pyrmaid(apex, b1, b2, b3, b4):
+    
+    apex = np.array(apex)
+    b1 = np.array(b1)
+    b2 = np.array(b2)
+    b3 = np.array(b3)
+    b4 = np.array(b4)
+
+    a12 = (b1 + b2)/2 - apex
+    a23 = (b2 + b3)/2 - apex
+    a34 = (b3 + b4)/2 - apex
+    a41 = (b4 + b1)/2 - apex
+    
+    ang1 = np.arccos( np.dot(a12,a34) /np.linalg.norm(a12)/np.linalg.norm(a34))  
+    ang2 = np.arccos( np.dot(a23,a41) /np.linalg.norm(a23)/np.linalg.norm(a41))  
+    return ang1, ang2
+    
+
+def sa_cone(angle):
+    """
+    Calculates the solid angle of a cone
+
+    Parameters
+    ----------
+    angle : float
+        The angle between the side of the cone and the center ray [rad].
+
+    Returns
+    -------
+    float
+        The solid angle in steridians.
+
+    """
+    return 2*np.pi *(1 - np.cos(angle))
+
 
 
 def xyx_a(ang1, ang2):
@@ -26,10 +63,20 @@ def xyx_a(ang1, ang2):
     
     return [x1,x2,x3,x4],[y1,y2,y3,y4], [z1,z2,z3,z4]
     
+def print_rad(angle):
+    print(f'{angle/np.pi:.5f}\N{GREEK SMALL LETTER PI}')
+
+
+print_rad(sa_cone(np.pi))
+
 
 x  = np.sqrt(3)*np.array([1, 1,-1,-1])
 y  = np.sqrt(3)*np.array([1,-1,-1, 1])
 z  = np.sqrt(3)*np.array([1,1,1,1])
+
+ang1, ang2 = da_pyrmaid([0,0,0], [1,1,1], [1,-1,1], [-1,-1,1], [-1,1,1])
+print_rad(sa_pyramid(ang1,ang2))
+
 
 sa1 = ag.pyramid_sa(x, y, z)
 
