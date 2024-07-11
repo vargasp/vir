27f23487dacdc901ee206894e4afa81d1f5b94b1
Phillip Vargas <vargasp@uchicago.edu>
2024-07-11 16:27:26 -0500

    mp testing update

--- tests/Test_MP/mp_test_main.py
+++ tests/Test_MP/mp_test_main.py
@@ -5,43 +5,23 @@ if filename and os.path.isfile(filename):
         startup_file = fobj.read()
     exec(startup_file)
 
-import matplotlib.pyplot as plt
 
 import numpy as np
-import time
-from multiprocessing import Pool, RawArray
-
 import mp_test_module as mp_mod
-import os, psutil
-
-import vir.mpct as mpct
-"""
-if __name__ == '__main__':
-    mp_mod.mp_process()
-
-"""
 
 
-
-m1 = mpct.ps_mem()
-#X_shape = (16, 1024*1024*32)
-X_shape = (16, 1024*32)
-
-# Randomly generate some data
-data = np.random.randn(*X_shape)
-print('Parent Data Address:',data.ctypes.data)
-
-m2 = mpct.ps_mem()
 
 if __name__ == '__main__':
+        
+    #X_shape = (16, 1024*1024*32)
+    X_shape = (16, 1024*32)
+    
+    # Randomly generate some data
+    data = np.random.randn(*X_shape)
+
+    
+    #Test function using a shared array that read only
     mp_mod.mp_unlock(data)
 
-mpct.ps_info(out='Parent', m=[m1,m2])
-
-
-
-"""
-if __name__ == '__main__':
+    #Test function using a shared array that read/write
     mp_mod.mp_lock(data)
-"""
-
