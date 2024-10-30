851be4ab642db58be1095a0fdc528b9cecb3bbfd
Phillip Vargas <vargasp@uchicago.edu>
2024-10-30 18:39:59 -0500

    Update affine_tansfrom_test1.py

--- tests/Test_Affine/affine_tansfrom_test1.py
+++ tests/Test_Affine/affine_tansfrom_test1.py
@@ -11,6 +11,38 @@ import matplotlib.pyplot as plt
 
 import vir.affine_transforms as af
 
+def scaleMat(coords,rank=None):
+    """
+    Creates an affine translation matrix for translation
+
+    Parameters
+    ----------
+    coords : float or arraylike
+        The translation vector 
+    rank : int, optional
+        The rank of the matrix. The default is "None" which is the lowest rank
+
+    Returns
+    -------
+    T : (rank, rank) np.array
+        The translation matrix in format: for coords = x; (x,y); and (x,y,z)
+        |1 x| if x   |1 0 x| if (x,y)   |1 0 0 x| if (x,y,z)
+        |0 1|        |0 1 y|            |0 1 0 y|
+                     |0 0 1|            |0 0 1 z|
+                                        |0 0 0 1|
+    """
+    #Calculates the dimension of the coords vector
+    coords = np.array(coords)
+    coords = np.append(coords,0)
+    
+    n = coords.size
+    
+    #Determines the rank of the matrix
+    if rank is None:
+        rank = n
+    
+    #Creates the tranlation matrix
+    return np.diag(coords)
 
 
 nX = 128
@@ -19,6 +51,11 @@ nZ = 64
 
 phantom2d = np.zeros([nX, nY])
 phantom2d[32:96,32:96] = 1
+
+
+
+
+
 coords = af.coords_array((nX,nY), ones=True)
 
 
@@ -27,7 +64,7 @@ angles_m = np.zeros(angles.size)
 
 
 for i, angle in enumerate(angles):
-    R = af.rotateMat((0,angle/100,0), center=(64,0))
+    R = af.rotateMat((0,angle/100,0), center=(120,0))
 
     R = np.linalg.inv(R)
     RC = (R @ coords)
