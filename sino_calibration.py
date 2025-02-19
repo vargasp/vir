92f074b8f9a4e3d506e98c8ba4b4cb0199f37e2b
Phillip Vargas <vargasp@uchicago.edu>
2025-02-19 09:06:54 -0600

    updatding structure

---
+++ sino_calibration.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 19:33:06 2025
+
+@author: pvargas21
+"""
+
+import numpy as np
+
+import vir
+import vir.affine_transforms as af
+
+
+def forward_project_phantom_misalign(phantom, Views, \
+                                     trans_X=0.0,trans_Y=0.0,trans_Z=0.0,
+                                     angX_Z=0.0, angY_Z=0.0, center_Z=None,\
+                                     angX_A=0.0, angY_A=0.0, center_A=None):
+    """
+    Creates a sinogram from a 3d phatom with a precession motion artifact. 
+
+    Parameters
+    ----------
+    phantom : (nX, nY, nZ) np.array
+        A 3d phantom.
+    Views : (nViews) np.array
+        The array of angles of rotation.
+    angX : float
+        The initial rotation angle in the X dimension.
+    angY : float
+        The initial rotation angle in the Y dimension.
+    center : (3) array_like, optional
+        The center of rotation location. The default is "None" which
+        corresponds to the center of the image.
+
+    Returns
+    -------
+    sino : (nAngs, nY, nZ) np.array
+        The forward projected sinogram with a precession artifact.
+    """
+
+    nX, nY, nZ = phantom.shape
+
+    if center_A is None:
+        center_A = (nX/2.-0.5, nY/2.-0.5, nZ/2.-0.5)
+
+    if center_Z is None:
+        center_Z = (nX/2.-0.5, nY/2.-0.5, nZ/2.-0.5)
+
+    
+    sino = np.zeros([Views.size,nZ,nX], dtype=np.float32)
+    coords = af.coords_array((nX,nY,nZ), ones=True)
+
+    #Misalign phantom by translating axis of rotation with respect to z-axis
+    T = af.transMat((trans_X,trans_Y,trans_Z))
+
+    #Misalign phantom by rotating axis of rotation with respect to z-axis
+    R_Z = af.rotateMat((angX_Z,angY_Z,0), center=center_Z, seq='XYZ')
+
+    #Loop through views
+    for i, view in enumerate(Views):
+
+        #Rotate for tomographic projection and add precession misalignment  
+        R_A = af.rotateMat((angX_A,angY_A,view), center=center_A, seq='XYZ')
+
+        #Apply transforms and project view
+        RRTC = np.round(R_A @ R_Z @ T @ coords,6)
+        sino[i,:,:] = af.coords_transform(phantom, RRTC).sum(axis=1).T
+    
+    return sino    
+
+
+def proj_orient_TM(r, x, center):
+    T = af.transMat((0,0,x))
+    R = af.rotateMat((r,0,0), center=center)
+    return T, R
+
+
+def precesion_TM(ang, phi, theta, center):
+    r = phi*np.cos(ang + theta)
+    z = np.sin(np.pi/2 - phi)
+    h_xy = np.cos(np.pi/2 - phi) 
+    
+    x = np.cos(ang + theta)*h_xy
+    s = np.sqrt(x**2 + z**2)
+
+    S = af.scaleMat((1,1/s,1))
+    R = af.rotateMat((r,0,0), center=center)
+    return S, R
+
+
+
+def calib_proj_orient_view(sino3d, Views, view, z,\
+                           transX=0.0, rZ=0.0, cenZ_y=None,\
+                           phi=0.0, theta=0.0, cenA_y=None, \
+                           transD=0.0, rD=0.0,\
+                           pitch=0):
+
+    nViews, nRows, nCols = sino3d.shape
+
+    #Creats coordinate arrays
+    coords = af.coords_array((1,nRows,nCols), ones=True)
+    coords[:,:,0,:] = np.interp(view,Views,np.arange(nViews))
+
+
+    #Center of of rotation transforms
+    if cenZ_y is None: cenZ_y = nRows/2.-0.5 - z
+    else: cenZ_y -= z
+        
+    center_Z = np.array((0.0, cenZ_y, nCols/2.-0.5))
+    T_Z, R_Z = proj_orient_TM(-rZ, -transX, center_Z)
+
+    
+    #Precession Transforms
+    if cenA_y is None: cenA_y = nRows/2.-0.5
+    else: cenA_y -= z    
+
+    center_A = np.array((0.0, cenA_y, nCols/2.-0.5))
+    S_A, R_A = precesion_TM(view, phi, theta, center_A)
+    
+    
+    #Detector Transforms     
+    center_D=(0.0,nRows/2-0.5,nCols/2-0.5)
+    T_D, R_D = proj_orient_TM(rD, transD, center_D)  
+
+
+    #Apply Transforms    
+    SRTR = np.linalg.inv(T_D @ R_D @ S_A @ R_A @ T_Z @ R_Z) @ coords
+    return np.squeeze(af.coords_transform(sino3d, SRTR))
+
+
+
+
+
+
+
+def calib_proj_orient(sino3d, Views, transX=0.0, rZ=0.0, cenZ_y=None,\
+                      phi=0.0, theta=0.0, cenA_y=None, transD=0.0, rD=0.0,\
+                      pitch=0):
+    nViews, nRows, nCols = sino3d.shape
+    
+    if cenA_y is None:
+        cenA_y = nRows/2.-0.5
+    center_A = np.array((0.0, cenA_y, nCols/2.-0.5))
+
+    if cenZ_y is None:
+        cenZ_y = nRows/2.-0.5
+    center_Z = np.array((0.0, cenZ_y, nCols/2.-0.5))
+    
+    center_D=(0.0,nRows/2-0.5,nCols/2-0.5)
+    T_D, R_D = proj_orient_TM(rD, transD, center_D)  
+    
+    
+    
+    dView = (Views[-1] - Views[0])/(nViews-1)
+    dZ = pitch*nRows*dView/(np.pi*2)
+    c = 128 - nRows/2 +1
+    Z = vir.censpace(nViews,c=c,d=dZ)
+    
+    coords = af.coords_array((1,nRows,nCols), ones=True)
+    #coords[:,:,0,:] = np.interp(ang,Angs,np.arange(nViews))
+    
+
+    for i in range(nViews):
+        coords[:,:,0,:] = i
+        view = Views[i]
+
+        #Center of of rotation transforms
+        center_Z[1] = cenZ_y-Z[i]
+        T_Z, R_Z = proj_orient_TM(-rZ, -transX, center_Z)
+
+        #Precession Transforms
+        center_A[1] = cenA_y-Z[i]
+        S_A, R_A = precesion_TM(view, phi, theta, center_A)
+        
+        SRTR = np.linalg.inv(T_D @ R_D @ S_A @ R_A @ T_Z @ R_Z) @ coords
+        sino3d[i,:,:] = np.squeeze(af.coords_transform(sino3d, SRTR))
+    
+    return sino3d
--- tests/Test_Sinogram/sinogram_test4.py
+++ tests/Test_Sinogram/sinogram_test4.py
@@ -20,122 +20,6 @@ def phantom2(f):
     return phantom
 
 
-def forward_project_phantom_misalign(phantom, Views, \
-                                     trans_X=0.0,trans_Y=0.0,trans_Z=0.0,
-                                     angX_Z=0.0, angY_Z=0.0, center_Z=None,\
-                                     angX_A=0.0, angY_A=0.0, center_A=None):
-    """
-    Creates a sinogram from a 3d phatom with a precession motion artifact. 
-
-    Parameters
-    ----------
-    phantom : (nX, nY, nZ) np.array
-        A 3d phantom.
-    Views : (nViews) np.array
-        The array of angles of rotation.
-    angX : float
-        The initial rotation angle in the X dimension.
-    angY : float
-        The initial rotation angle in the Y dimension.
-    center : (3) array_like, optional
-        The center of rotation location. The default is "None" which
-        corresponds to the center of the image.
-
-    Returns
-    -------
-    sino : (nAngs, nY, nZ) np.array
-        The forward projected sinogram with a precession artifact.
-    """
-
-    nX, nY, nZ = phantom.shape
-
-    if center_A is None:
-        center_A = (nX/2.-0.5, nY/2.-0.5, nZ/2.-0.5)
-
-    if center_Z is None:
-        center_Z = (nX/2.-0.5, nY/2.-0.5, nZ/2.-0.5)
-
-    
-    sino = np.zeros([Views.size,nZ,nX], dtype=np.float32)
-    coords = af.coords_array((nX,nY,nZ), ones=True)
-
-    #Misalign phantom by translating axis of rotation with respect to z-axis
-    T = af.transMat((trans_X,trans_Y,trans_Z))
-
-    #Misalign phantom by rotating axis of rotation with respect to z-axis
-    R_Z = af.rotateMat((angX_Z,angY_Z,0), center=center_Z, seq='XYZ')
-
-    #Loop through views
-    for i, view in enumerate(Views):
-
-        #Rotate for tomographic projection and add precession misalignment  
-        R_A = af.rotateMat((angX_A,angY_A,view), center=center_A, seq='XYZ')
-
-        #Apply transforms and project view
-        RRTC = np.round(R_A @ R_Z @ T @ coords,6)
-        sino[i,:,:] = af.coords_transform(phantom, RRTC).sum(axis=1).T
-    
-    return sino    
-
-
-def calib_proj_orient_TM(r, x, center):
-    T = af.transMat((0,0,x))
-    R = af.rotateMat((r,0,0), center=center)
-    return T, R
-
-
-def calib_precesion_TM(ang, phi, theta, center):
-    r = phi*np.cos(ang + theta)
-    z = np.sin(np.pi/2 - phi)
-    h_xy = np.cos(np.pi/2 - phi) 
-    
-    x = np.cos(ang + theta)*h_xy
-    s = np.sqrt(x**2 + z**2)
-
-    S = af.scaleMat((1,1/s,1))
-    R = af.rotateMat((r,0,0), center=center)
-    return S, R
-
-
-def calib_proj_orient(sino3d, Angs, transX=0.0, rZ=0.0, cenZ_y=None,\
-                      phi=0.0, theta=0.0, cenA_y=None, pitch=0):
-    nViews, nRows, nCols = sino3d.shape
-    
-    if cenA_y is None:
-        cenA_y = nRows/2.-0.5
-    center_A = np.array((0.0, cenA_y, nCols/2.-0.5))
-
-    if cenZ_y is None:
-        cenZ_y = nRows/2.-0.5
-    center_Z = np.array((0.0, cenZ_y, nCols/2.-0.5))
-    
-    dView = (Views[-1] - Views[0])/(nViews-1)
-    dZ = pitch*nRows*dView/(np.pi*2)
-    c = 128 - nRows/2 +1
-    Z = vir.censpace(nViews,c=c,d=dZ)
-    
-    coords = af.coords_array((1,nRows,nCols), ones=True)
-    #coords[:,:,0,:] = np.interp(ang,Angs,np.arange(nViews))
-    
-
-    for i in range(nViews):
-        coords[:,:,0,:] = i
-        ang = Angs[i]
-
-        #Center of of rotation transforms
-        center_Z[1] = cenZ_y-Z[i]
-        T_Z, R_Z = calib_proj_orient_TM(-rZ, -transX, center_Z)
-
-        #Precession Transforms
-        center_A[1] = cenA_y-Z[i]
-        S_A, R_A = calib_precesion_TM(ang, phi, theta, center_A)
-        
-        SRTR = np.linalg.inv(S_A @ R_A @ T_Z @ R_Z) @ coords
-        sino3d[i,:,:] = np.squeeze(af.coords_transform(sino3d, SRTR))
-    
-    return sino3d
-
-
 def sino_par2hel(sinoP, nRows, nAngs, pitch, transD=0.0, rD=0.0):
     nViews, nRowsP, nCols = sinoP.shape
     
@@ -144,8 +28,6 @@ def sino_par2hel(sinoP, nRows, nAngs, pitch, transD=0.0, rD=0.0):
     c =nRowsP/2 - nRows/2 +1
     Z = vir.censpace(nViews,c=c,d=dZ)
 
-    center=(0.0,nRows/2-0.5,nCols/2-0.5)
-    T, R = calib_proj_orient_TM(rD, transD, center)  
     sinoH = np.zeros([Views.size,nRows,nCols], dtype=np.float32)
 
 
@@ -154,10 +36,11 @@ def sino_par2hel(sinoP, nRows, nAngs, pitch, transD=0.0, rD=0.0):
         coords[:,:,0,:] = i
         coords[:,:,1,:] = coords[:,:,1,:] + Z[i]
         
-        #T = af.transMat((-i,-Z[i],0))
+        center=(0.0,Z[i]+nRows/2-0.5,nCols/2-0.5)
+        T, R = proj_orient_TM(rD, transD, center)  
+
         RTC = (np.linalg.inv(R @ T) @ coords)
-        #RTC = (np.linalg.inv(R) @ coords)
-    
+
         sinoH[i,:,:] = af.coords_transform(sinoP, np.round(RTC,6))
 
     return sinoH
@@ -235,12 +118,8 @@ view_sinos(sino2C, sino)
 
 
 """#Helical"""
-sinoH10 = sino_par2hel(sino, 128, nAngs, 1, transD=50,rD=.2)
-view_sinos(sinoH10)
-sinoH0 = sino_par2hel(sino, 128, nAngs, 1, transD=0,rD=0)
-view_sinos(sinoH0)
-
-view_sinos(sinoH10, sinoH0)
+sinoH = sino_par2hel(sino, 128, nAngs, 1)
+view_sinos(sinoH)
 
 
 """Axis of rotaion, rotated with respect to z-axis"""
@@ -267,4 +146,34 @@ sinoH2C = calib_proj_orient(sinoH2.copy(), Views, transX=-trans_X,\
 view_sinos(sinoH2C, sinoH)
 
 
+"""Dectector offset"""
+transD=5.5
+sinoH_d0 = sino_par2hel(sino, 128, nAngs, 1, transD=transD)
+sinoHd0C = calib_proj_orient(sinoH_d0.copy(), Views, transD=-transD, pitch=1)
+view_sinos(sinoHd0C, sinoH)
+
+
+"""Dectector rotation"""
+rD=0.075
+sinoH_d1 = sino_par2hel(sino, 128, nAngs, 1, rD=rD)
+sinoHd1C = calib_proj_orient(sinoH_d1.copy(), Views, rD=-rD, pitch=1)
+view_sinos(sinoHd1C, sinoH)
+
+
+"""Dectector offset, Dectector rotation"""    
+sinoH_d2 = sino_par2hel(sino, 128, nAngs, 1, transD=transD,rD=rD)
+sinoHd2C = calib_proj_orient(sinoH_d2.copy(), Views, transD=-transD, rD=-rD,\
+                             pitch=1)
+view_sinos(sinoHd2C, sinoH)
+
+
+"""Axis of rotaion - rotated and translated with respect to z-axis and precessing"""
+"""Dectector offset, Dectector rotation"""    
+sinoH2_d2 = sino_par2hel(sino2, 128, nAngs, 1, transD=transD,rD=rD)
+sinoH2_d2C = calib_proj_orient(sinoH2_d2.copy(), Views, transD=-transD, rD=-rD,\
+                               transX=-trans_X,\
+                               rZ=-angY_Z,cenZ_y=center_Z[2],\
+                               phi=angY_A,cenA_y=center_A[2],\
+                               pitch=1)
+view_sinos(sinoH2_d2C, sinoH)
 
