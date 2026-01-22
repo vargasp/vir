33ac00fe5e1f1f7bdff500e032687895cff91191
Phillip Vargas <vargasp@uchicago.edu>
2026-01-22 15:48:06 -0600

    Update pd.py

--- sys_mat/pd.py
+++ sys_mat/pd.py
@@ -7,6 +7,14 @@ Created on Mon Jan 19 15:27:50 2026
 
 import numpy as np
 
+def proj_img2det_fan(px, py, ox, oy, DSO, DSD):
+    return DSD * (px + py) / (DSO - (ox + oy))
+
+
+def proj_object2det_par(px, py):
+    return px + py
+
+
 def pd_fp_par_2d(img, ang_arr, nu, du=1.0, su=0.0, d_pix=1.0):
     """
     Separable footprints forward projector for 2D parallel-beam CT.
@@ -27,12 +35,12 @@ def pd_fp_par_2d(img, ang_arr, nu, du=1.0, su=0.0, d_pix=1.0):
     nx, ny = img.shape
     sino = np.zeros((ang_arr.size, nu), dtype=np.float32)
 
-    # Image edges (centered)
+    # Pixel boundaries (image centered at origin)
     x_bnd_arr = d_pix*(np.arange(nx+1, dtype=np.float32) - nx/2)
     y_bnd_arr = d_pix*(np.arange(ny+1, dtype=np.float32) - ny/2)
 
-    # Detector edges:
-    u_bnd_arr = du*(np.arange(nu + 1, dtype=np.float32) - nu/2.0 + su)
+    # Detector bin boundaries (u)
+    u_bnd_arr = du*(np.arange(nu + 1, dtype=np.float32) - nu/2 + su)
 
     # Precompute trig functions for all angles
     cos_ang_arr = np.cos(ang_arr)
@@ -40,39 +48,42 @@ def pd_fp_par_2d(img, ang_arr, nu, du=1.0, su=0.0, d_pix=1.0):
 
     for ia, (cos_ang,sin_ang) in enumerate(zip(cos_ang_arr,sin_ang_arr)):
         
-        #Projection of the corners of the pixel on the detector axis:
-        #-sin_ang*x + cos_ang*y
-        #-px + py
+        #Rotates the x,y coordinate system to o,p (orthogonal and parallel to
+        #the detector)
+        #In parallel beam these coordinates will projection directly on the 
+        #detector with p(x,y = -sin_ang*x + cos_ang*y
+
+        #Precomputes the components
         px_bnd_arr = -sin_ang*x_bnd_arr
+        py_bnd_arr =  cos_ang*y_bnd_arr
         
+        #Loops through the precomputed x cooridnates  
         px_bnd_l = px_bnd_arr[0]
         for ix, px_bnd_r in enumerate(px_bnd_arr[1:]):
+                                    
+            # Initalize first boundary
+            py_bnd_l = py_bnd_arr[0]    
             
-            #Projection of the corners of the pixel on the detector axis:
-            #-sin_ang*x + cos_ang*y
-            #-px + py
-            py_bnd_arr = cos_ang*y_bnd_arr
-                        
-            
-            py_bnd_l = py_bnd_arr[0]           
+            #Calulates the first two pixel corners projected on detector
             p0 = py_bnd_l + px_bnd_l
             p1 = py_bnd_l + px_bnd_r
             
+            #Loops through the precomputed y cooridnates  
             for iy, py_bnd_r in enumerate(py_bnd_arr[1:]):
+                #Calulates the second two pixel corners projected on detector
                 p2 = py_bnd_r + px_bnd_l
                 p3 = py_bnd_r + px_bnd_r
 
+                #If image pixel is 0 advance to next pixel
                 if img[ix, iy] == 0:
+                    py_bnd_l = py_bnd_r
                     p0 = p2
                     p1 = p3
-                    py_bnd_l = py_bnd_r
                     continue
 
-                # Project pixel corners to detector axis
-                corners = [p0, p1, p2, p3]
-
-                P_min = min(corners)
-                P_max = max(corners)
+                #min and 
+                P_min = min([p0, p1, p2, p3])
+                P_max = max([p0, p1, p2, p3])
 
                 # Footprint: find detector bins that overlap with rectangle projection
 
@@ -94,10 +105,7 @@ def pd_fp_par_2d(img, ang_arr, nu, du=1.0, su=0.0, d_pix=1.0):
 
             px_bnd_l = px_bnd_r
 
-
     return sino
-
-
 
 
 def pd_fp_fan_2d(img, ang_arr, nu, DSO, DSD, du=1.0, su=0.0, d_pix=1.0):
@@ -112,56 +120,47 @@ def pd_fp_fan_2d(img, ang_arr, nu, DSO, DSD, du=1.0, su=0.0, d_pix=1.0):
     y_bnd_arr = d_pix * (np.arange(ny + 1) - ny / 2)
 
     # Detector bin boundaries (u)
-    u_bnd = du * (np.arange(nu + 1) - nu / 2 + su)
+    u_bnd = du * (np.arange(nu + 1) - nu/2 + su)
 
     # Precompute trig functions for all angles
     cos_ang_arr = np.cos(ang_arr)
     sin_ang_arr = np.sin(ang_arr)
     
     for ia, (cos_ang,sin_ang) in enumerate(zip(cos_ang_arr,sin_ang_arr)):
-    
-        px_bnd_arr = -sin_ang*x_bnd_arr
-        ox_bnd_arr =  cos_ang*x_bnd_arr
+            
+        #Rotates the x,y coordinate system to o,p (orthogonal and parallel to
+        #the detector)
+        #In parallel beam these coordinates will projection directly on the 
+        #detector with p(x,y = -sin_ang*x + cos_ang*y
 
+        #Precomputes the components    
+        px_bnd_arr = -sin_ang*x_bnd_arr
         py_bnd_arr =  cos_ang*y_bnd_arr
+
+        ox_bnd_arr =  cos_ang*x_bnd_arr
         oy_bnd_arr =  sin_ang*y_bnd_arr
     
+        #Loops through the precomputed x cooridnates  
         px_bnd_l = px_bnd_arr[0]
-        ox_bnd_l = ox_bnd_arr[0]
-        
+        ox_bnd_l = ox_bnd_arr[0]        
         for ix, (px_bnd_r,ox_bnd_r) in enumerate(zip(px_bnd_arr[1:],ox_bnd_arr[1:])):
     
-            #Projection of the corners of the pixel on the detector axis:
-            #-sin_ang*x + cos_ang*y
-            #-px + py
-            #px_bnd_l = -sin_ang*x_bnd_l
-            #px_bnd_r = -sin_ang*x_bnd_r
-
-            #Projection of the corners of the pixel on the orthoganal 
-            #to the detector axis:
-            #cos_ang*x + sin_ang*y
-            #ox + oy  
-            #ox_bnd_l = cos_ang*x_bnd_l
-            #ox_bnd_r = cos_ang*x_bnd_r
-
-                
+            # Initalize first boundary
             py_bnd_l = py_bnd_arr[0]
             oy_bnd_l = oy_bnd_arr[0]
             
-            denom = DSO - (ox_bnd_l + oy_bnd_l)
-            p0 = DSD * (px_bnd_l + py_bnd_l) / denom
+            #Calulates the first two pixel corners projected on detector
+            p0 = proj_img2det_fan(px_bnd_l, py_bnd_l, ox_bnd_l, oy_bnd_l, DSO, DSD)
+            p1 = proj_img2det_fan(px_bnd_r, py_bnd_l, ox_bnd_r, oy_bnd_l, DSO, DSD)
 
-            denom = DSO - (ox_bnd_r + oy_bnd_l)
-            p1 = DSD * (px_bnd_r + py_bnd_l) / denom
-
-            
+            #Loops through the precomputed y cooridnates              
             for iy, (py_bnd_r,oy_bnd_r) in enumerate(zip(py_bnd_arr[1:],oy_bnd_arr[1:])):
 
-                denom = DSO - (ox_bnd_l + oy_bnd_r)
-                p2 = DSD * (px_bnd_l + py_bnd_r) / denom
-
-                denom = DSO - (ox_bnd_r + oy_bnd_r)
-                p3 = DSD * (px_bnd_r + py_bnd_r) / denom
+                #Calulates the second two pixel corners projected on detector    
+                p2 = proj_img2det_fan(px_bnd_l, py_bnd_r, ox_bnd_l, oy_bnd_r, DSO, DSD)
+                p3 = proj_img2det_fan(px_bnd_r, py_bnd_r, ox_bnd_r, oy_bnd_r, DSO, DSD)
+    
+    
     
                 val = img[ix, iy]
                 if val == 0:
