64f6eafd403401199e8f6fb84ab4c87f9006f72a
Phillip Vargas <vargasp@uchicago.edu>
2026-01-21 21:07:30 -0600

    Update pd.py

--- sys_mat/pd.py
+++ sys_mat/pd.py
@@ -40,24 +40,36 @@ def pd_fp_par_2d(img, ang_arr, nu, du=1.0, su=0.0, d_pix=1.0):
 
     for ia, (cos_ang,sin_ang) in enumerate(zip(cos_ang_arr,sin_ang_arr)):
         
-        x_bnd_l = x_bnd_arr[0]
-        for ix, x_bnd_r in enumerate(x_bnd_arr[1:]):
-            p_bnd_l = -sin_ang*x_bnd_l
-            p_bnd_r = -sin_ang*x_bnd_r
+        #Projection of the corners of the pixel on the detector axis:
+        #-sin_ang*x + cos_ang*y
+        #-px + py
+        px_bnd_arr = -sin_ang*x_bnd_arr
+        
+        px_bnd_l = px_bnd_arr[0]
+        for ix, px_bnd_r in enumerate(px_bnd_arr[1:]):
             
-            y_bnd_l = y_bnd_arr[0]                
-            for iy, y_bnd_r in enumerate(y_bnd_arr[1:]):
-                o_bnd_l = cos_ang*y_bnd_l
-                o_bnd_r = cos_ang*y_bnd_r
-                
+            #Projection of the corners of the pixel on the detector axis:
+            #-sin_ang*x + cos_ang*y
+            #-px + py
+            py_bnd_arr = cos_ang*y_bnd_arr
+                        
+            
+            py_bnd_l = py_bnd_arr[0]           
+            p0 = py_bnd_l + px_bnd_l
+            p1 = py_bnd_l + px_bnd_r
+            
+            for iy, py_bnd_r in enumerate(py_bnd_arr[1:]):
+                p2 = py_bnd_r + px_bnd_l
+                p3 = py_bnd_r + px_bnd_r
 
                 if img[ix, iy] == 0:
-                    y_bnd_l = y_bnd_r
+                    p0 = p2
+                    p1 = p3
+                    py_bnd_l = py_bnd_r
                     continue
 
                 # Project pixel corners to detector axis
-                corners = [o_bnd_l + p_bnd_l, o_bnd_r + p_bnd_l,
-                           o_bnd_l + p_bnd_r, o_bnd_r + p_bnd_r]
+                corners = [p0, p1, p2, p3]
 
                 P_min = min(corners)
                 P_max = max(corners)
@@ -76,8 +88,11 @@ def pd_fp_par_2d(img, ang_arr, nu, du=1.0, su=0.0, d_pix=1.0):
                         # Normalize by bin width
                         sino[ia, iu] += img[ix, iy] * (right - left) / du
 
-                y_bnd_l = y_bnd_r
-            x_bnd_l = x_bnd_r
+                py_bnd_l = py_bnd_r
+                p0 = p2
+                p1 = p3
+
+            px_bnd_l = px_bnd_r
 
 
     return sino
@@ -105,34 +120,59 @@ def pd_fp_fan_2d(img, ang_arr, nu, DSO, DSD, du=1.0, su=0.0, d_pix=1.0):
     
     for ia, (cos_ang,sin_ang) in enumerate(zip(cos_ang_arr,sin_ang_arr)):
     
-        x_bnd_l = x_bnd_arr[0]
-        for ix, x_bnd_r in enumerate(x_bnd_arr[1:]):
+        px_bnd_arr = -sin_ang*x_bnd_arr
+        ox_bnd_arr =  cos_ang*x_bnd_arr
+
+        py_bnd_arr =  cos_ang*y_bnd_arr
+        oy_bnd_arr =  sin_ang*y_bnd_arr
     
+        px_bnd_l = px_bnd_arr[0]
+        ox_bnd_l = ox_bnd_arr[0]
+        
+        for ix, (px_bnd_r,ox_bnd_r) in enumerate(zip(px_bnd_arr[1:],ox_bnd_arr[1:])):
+    
+            #Projection of the corners of the pixel on the detector axis:
+            #-sin_ang*x + cos_ang*y
+            #-px + py
+            #px_bnd_l = -sin_ang*x_bnd_l
+            #px_bnd_r = -sin_ang*x_bnd_r
+
+            #Projection of the corners of the pixel on the orthoganal 
+            #to the detector axis:
+            #cos_ang*x + sin_ang*y
+            #ox + oy  
+            #ox_bnd_l = cos_ang*x_bnd_l
+            #ox_bnd_r = cos_ang*x_bnd_r
+
                 
-            y_bnd_l = y_bnd_arr[0]                
-            for iy, y_bnd_r in enumerate(y_bnd_arr[1:]):
+            py_bnd_l = py_bnd_arr[0]
+            oy_bnd_l = oy_bnd_arr[0]
+            
+            denom = DSO - (ox_bnd_l + oy_bnd_l)
+            p0 = DSD * (px_bnd_l + py_bnd_l) / denom
+
+            denom = DSO - (ox_bnd_r + oy_bnd_l)
+            p1 = DSD * (px_bnd_r + py_bnd_l) / denom
+
+            
+            for iy, (py_bnd_r,oy_bnd_r) in enumerate(zip(py_bnd_arr[1:],oy_bnd_arr[1:])):
+
+                denom = DSO - (ox_bnd_l + oy_bnd_r)
+                p2 = DSD * (px_bnd_l + py_bnd_r) / denom
+
+                denom = DSO - (ox_bnd_r + oy_bnd_r)
+                p3 = DSD * (px_bnd_r + py_bnd_r) / denom
     
                 val = img[ix, iy]
                 if val == 0:
-                    y_bnd_l = y_bnd_r
+                    py_bnd_l = py_bnd_r
+                    oy_bnd_l = oy_bnd_r
+                    p0 = p2
+                    p1 = p3
                     continue
 
-                
-                denom = DSO - (x_bnd_l * cos_ang + y_bnd_l * sin_ang)
-                u1 = DSD * (-x_bnd_l * sin_ang + y_bnd_l * cos_ang) / denom
-
-                denom = DSO - (x_bnd_r * cos_ang + y_bnd_l * sin_ang)
-                u2 = DSD * (-x_bnd_r * sin_ang + y_bnd_l * cos_ang) / denom
-
-                denom = DSO - (x_bnd_l * cos_ang + y_bnd_r * sin_ang)
-                u3 = DSD * (-x_bnd_l * sin_ang + y_bnd_r * cos_ang) / denom
-
-                denom = DSO - (x_bnd_r * cos_ang + y_bnd_r * sin_ang)
-                u4 = DSD * (-x_bnd_r * sin_ang + y_bnd_r * cos_ang) / denom
-
-
-                umin = min([u1,u2,u3,u4])
-                umax = max([u1,u2,u3,u4])
+                umin = min([p0,p1,p2,p3])
+                umax = max([p0,p1,p2,p3])
 
 
                 
@@ -146,8 +186,13 @@ def pd_fp_fan_2d(img, ang_arr, nu, DSO, DSD, du=1.0, su=0.0, d_pix=1.0):
                     if right > left:
                         sino[ia, iu] += val * (right - left) / du
 
-                y_bnd_l = y_bnd_r
-            x_bnd_l = x_bnd_r
+                py_bnd_l = py_bnd_r
+                oy_bnd_l = oy_bnd_r
+                p0 = p2
+                p1 = p3
+
+            px_bnd_l = px_bnd_r
+            ox_bnd_l = ox_bnd_r
 
     return sino
 
