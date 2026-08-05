4899b2afe621a8132d5fc285aaaccf70f813606b
Phillip Vargas <vargasp@uchicago.edu>
2026-08-05 16:28:24 -0500

    vos update

---
+++ test_vos.py
@@ -0,0 +1,23 @@
+import vir.vos as vos
+
+
+fnames = ['/home/uname1/folder1/test1.tst',
+          '/Users/uname1/folder1/test1.tst',
+          'C:\\Users\\uname1\\folder1\\test1.tst',
+          '~/Desktop/test1.tst',
+          'folder1/test1.tst',
+          'folder1\\test1.tst',
+          'box/folder1/test1.tst',
+          'box\\folder1\\test1.tst',
+          'Box/folder1/test1.tst',
+          'Box\\folder1\\test1.tst'
+          '/Users/uname1/Library/CloudStorage/Box-Box/folder1/test1.tst',
+          'C:\\Users\\uname1\\Box\\folder1\\test1.tst']
+
+
+for fname in fnames:
+    print(fname, "->",vos.file_path(fname))
+
+for fname in fnames:
+    print(fname, "->",vos.file_path(fname,box_root=True))
+
--- vos.py
+++ vos.py
@@ -11,6 +11,8 @@ Created on Thu Dec  8 16:23:54 2022
 import os
 import re
 
+"""
+
 def file_path(fname):
     home_dir = os.path.expanduser('~')
     #work_dir = os.getcwd()
@@ -26,7 +28,9 @@ def file_path(fname):
     
     return os.path.join(home_dir,*dirs)
 
-    
+"""
+
+
 def box_dir(research_folder=True):
 
     box_drive_locations = []
@@ -54,3 +58,111 @@ def box_dir(research_folder=True):
 
     return ''
 
+
+from pathlib import Path, PurePosixPath, PureWindowsPath
+
+def find_box_root():
+    home = Path.home()
+
+    candidates = [
+        home / "Box",
+        home / "Library" / "CloudStorage" / "Box-Box",   # macOS
+        home / "Box Sync",                               # older installs
+    ]
+
+    for path in candidates:
+        if path.exists():
+            return path
+
+    return None
+
+BOX_ROOT = find_box_root()
+
+
+def file_path(fname):
+    """Return a path appropriate for the current OS."""
+
+    home = Path.home()
+
+    # ~/...
+    if fname.startswith("~"):
+        return str(Path(fname).expanduser())
+
+    # /home/<user>/...
+    posix = PurePosixPath(fname)
+    if len(posix.parts) >= 3 and posix.parts[:2] == ("/", "home"):
+        return str(home.joinpath(*posix.parts[3:]))
+
+    # C:\Users\<user>\...
+    win = PureWindowsPath(fname)
+    if (win.drive and
+            len(win.parts) >= 4 and
+            win.parts[1].lower() == "users"):
+        return str(home.joinpath(*win.parts[3:]))
+
+
+
+
+    fname = fname.replace("\\", "/")
+    return str(Path(*PurePosixPath(fname).parts))
+
+
+        # Relative path (or anything else): just normalize separators
+    #parts = [p for p in win.parts if p not in (win.drive, "\\", "/")]
+    #return str(Path(*parts))
+
+
+
+def file_path(fname, box_root=False):
+    """
+    Convert a path to one appropriate for the current operating system.
+
+    Supports:
+
+        ~/...
+        /home/<user>/...
+        C:\\Users\\<user>\\...
+        box/...
+        box\\...
+        relative/path
+        relative\\path
+    """
+
+    home = Path.home()
+
+    # Normalize separators for parsing
+    normalized = fname.replace("\\", "/")
+    posix = PurePosixPath(normalized)
+
+    if normalized.startswith("~"):
+        return str(Path(normalized).expanduser())
+
+    # box/...
+    if box_root==True:
+        if BOX_ROOT is None:
+            raise FileNotFoundError("Could not locate the user's Box folder.")
+        return str(BOX_ROOT.joinpath(*posix.parts))
+
+    if posix.parts[0].lower() == "box":
+        if BOX_ROOT is None:
+            raise FileNotFoundError("Could not locate the user's Box folder.")
+        return str(BOX_ROOT.joinpath(*posix.parts[1:]))
+
+
+    # /home/<user>/...
+    if len(posix.parts) >= 3 and posix.parts[:2] == ("/", "home"):
+        return str(home.joinpath(*posix.parts[3:]))
+
+    # C:\Users\<user>\...
+    win = PureWindowsPath(fname)
+    if (
+        win.drive
+        and len(win.parts) >= 4
+        and win.parts[1].lower() == "users"
+    ):
+        return str(home.joinpath(*win.parts[3:]))
+
+    # ------------------------------------------------------------------
+    # Relative path
+    # ------------------------------------------------------------------
+    return str(Path(*posix.parts))
